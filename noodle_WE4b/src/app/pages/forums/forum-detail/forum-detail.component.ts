c0ffdcdab052bcbd1fa0ca93130c3b4dc25c2409
Swann <development.swann@gmail.com>
2025-06-25 19:37:23 +0200

    log connexion

--- noodle_WE4b/src/app/pages/forums/forum-detail/forum-detail.component.ts
+++ noodle_WE4b/src/app/pages/forums/forum-detail/forum-detail.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { ForumService } from '../../../services/forum.service';
 import { UtilisateurService } from '../../../services/utilisateur.service';
 import { AuthService } from '../../../services/auth.service';
+import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-forum-detail',
@@ -53,7 +55,10 @@ export class ForumDetailComponent implements OnInit {
     private route: ActivatedRoute,
     private forumService: ForumService,
     private utilisateurService: UtilisateurService,
-    private authService: AuthService
+    private authService: AuthService,
+    private location: Location
+
+
   ) {}
 
   ngOnInit() {
@@ -571,5 +576,8 @@ export class ForumDetailComponent implements OnInit {
     const currentUserId = this.getCurrentUserId();
     // L'auteur peut supprimer sa propre réponse OU les profs/admins peuvent supprimer toutes les réponses
     return (reply.userId === currentUserId && !!currentUserId) || this.canModerate();
+  }
+  goBack() {
+    this.location.back();
   }
 }
