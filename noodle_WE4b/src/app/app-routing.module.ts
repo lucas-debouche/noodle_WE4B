fa3c442f13be6879dfd3dfa692d686dce6660495
thibault <thibaultb04@gmail.com>
2025-06-28 22:27:16 +0200

    ajout de la route pour relier le bouton modif user

--- noodle_WE4b/src/app/app-routing.module.ts
+++ noodle_WE4b/src/app/app-routing.module.ts
@@ -27,6 +27,7 @@ const routes: Routes = [
 
   // Admin : création d'utilisateurs
   { path: 'admin/users-registration', component: UserRegistrationComponent, canActivate: [AuthAdminGuard] },
+  { path: 'admin/users-registration/:id', component: UserRegistrationComponent, canActivate: [AuthGuard] },
 
   // Admin : enregistrement des UE
   { path: 'admin/ue-registration/:id', component: UeRegistrationComponent, canActivate: [AuthAdminGuard] },
